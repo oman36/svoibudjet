967fbb7a9465a1c88cd3bad9d530dfdbb597099b
Vladimir <neoman36@gmail.com>
2018-06-14 07:23:27 +0400

    Models was updated

--- srv/svoibudjet/app/models.py
+++ srv/svoibudjet/app/models.py
@@ -1,24 +1,50 @@
 from django.db import models
 
 
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    inn = models.BigIntegerField(unique=True)
+    created_at = models.DateTimeField(null=False, auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(null=False, auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(null=False, auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(null=False, auto_now=True)
+
+    def __str__(self):
+        return '[%s] %s' % (self.shop.name, self.name,)
+
+    class Meta:
+        unique_together = ('shop', 'name',)
+
+
 class Check(models.Model):
-    date = models.DateTimeField(null=False)
+    date = models.DateTimeField(null=False, unique=True)
     discount = models.FloatField(default=0)
     discount_sum = models.FloatField(default=0)
     total_sum = models.IntegerField(null=False)
-    user = models.CharField(max_length=255)
-    user_inn = models.IntegerField()
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(null=False, auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(null=False, auto_now=True)
 
     def __str__(self):
-        return 'Sum: %s [%s]' % (self.total_sum, self.date.isoformat())
+        return '%s %s' % (self.shop.__str__(), self.date.isoformat())
 
 
 class Item(models.Model):
     check_model = models.ForeignKey(Check, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.FloatField(null=False)
     quantity = models.FloatField(null=False)
     sum = models.FloatField(null=False)
+    created_at = models.DateTimeField(null=False, auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(null=False, auto_now=True)
 
     def __str__(self):
-        return '%s  %d * %d' % (self.name, self.quantity, self.price)
+        return '%s  %.3f x %.2f' % (self.product.name, self.quantity, self.price)
