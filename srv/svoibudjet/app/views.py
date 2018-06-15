045315c2a03a45d77d0672280d197b2b850df7c5
Vladimir <neoman36@gmail.com>
2018-06-15 22:28:59 +0400

    Views was added

---
+++ srv/svoibudjet/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Check
+
+app_name = 'app'
+
+
+def index(request):
+    checks = Check.objects.order_by('-date')
+    total = 0
+    for check in checks:
+        total += check.total_sum
+
+    return render(request, 'app/index.html', {
+        'checks': checks,
+        'total': total / 100
+    })
